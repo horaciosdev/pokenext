35fbe06f450c89a4db60276958cb49fcc80bd759
Horacio <horacio.schumann@gmail.com>
2023-02-14 14:20:48 -0300

    feat: criado componente Card

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import styles from "@/styles/Home.module.css";
 import Image from "next/image";
 
+import Card from "./components/Card";
+
 export async function getStaticProps() {
   const maxPokemons = 251;
   const api = "https://pokeapi.co/api/v2/pokemon/";
@@ -36,7 +38,7 @@ export default function Home({ pokemons }) {
       </div>
       <div className={styles.pokemon_container}>
         {pokemons.map((pokemon) => (
-          <p key={pokemon.id}>{pokemon.name}</p>
+          <Card key={pokemon.id} pokemon={pokemon} />
         ))}
       </div>
     </>
