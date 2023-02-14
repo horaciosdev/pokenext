8690fd2b0a4bfe8be8c9505ac4afc4f48d170938
Horacio <horacio.schumann@gmail.com>
2023-02-14 14:48:09 -0300

    fix: pasta components movida para o diretório raiz

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import styles from "@/styles/Home.module.css";
 import Image from "next/image";
 
-import Card from "./components/Card";
+import Card from "../components/Card";
 
 export async function getStaticProps() {
   const maxPokemons = 251;
